9eff7d3fcad2f00f4588dd9b2b00465003a70b0b
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-03-14 15:52:09 +0900

    color & label change

--- Change Color/ViewController.swift
+++ Change Color/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+        var flag = 0
+    
+    @IBOutlet weak var tqet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
     }
-
+    @IBAction func fgjdfs(_ sender: Any) {
+        if flag == 0{view.backgroundColor = UIColor.green
+            flag = 1
+            tqet.text = "green"
+        }
+        else if flag == 1 {view.backgroundColor = UIColor.yellow
+            flag = 0
+            tqet.text = "yellow"
+    }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +36,3 @@ class ViewController: UIViewController {
 
 
 }
-
